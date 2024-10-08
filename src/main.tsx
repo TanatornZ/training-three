ecbdaa07fbc854a37dfdb5f84d2229d7fea209cf
TanatornZ <bank250145.bb@gmail.com>
2024-10-08 16:31:58 +0700

    feat: init circle example

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./modules/App.tsx";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CirclePage from "./modules/circle/index.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/circle" element={<CirclePage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
