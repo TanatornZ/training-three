e76b3fd822bc820d9fc555e6fa0a3a74301f515f
TanatornZ <bank250145.bb@gmail.com>
2024-10-09 11:49:38 +0700

    feat: convert original code to suit with react

--- src/modules/circle/index.tsx
+++ src/modules/circle/index.tsx
@@ -1,146 +1,156 @@
 import * as THREE from "three";
 import { GUI } from "three/addons/libs/lil-gui.module.min.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 function CirclePage() {
-  useEffect(() => {
-    const params = {
-      clipIntersection: true,
-      planeConstant: 0,
-      showHelpers: false,
-      alphaToCoverage: true,
-    };
+  const canvas = useRef<HTMLCanvasElement>(null!);
 
+  const params = {
+    clipIntersection: true,
+    planeConstant: 0,
+    showHelpers: false,
+    alphaToCoverage: true,
+  };
+
+  useEffect(() => {
+    if (!canvas.current) return;
+
+    const renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      canvas: canvas.current,
+    });
+
+    function render() {
+      renderer.render(scene, camera);
+    }
+
+    // create helpers line
     const clipPlanes = [
       new THREE.Plane(new THREE.Vector3(1, 0, 0), 0),
       new THREE.Plane(new THREE.Vector3(0, -1, 0), 0),
       new THREE.Plane(new THREE.Vector3(0, 0, -1), 0),
     ];
 
-    function init() {
-      const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.localClippingEnabled = true;
+    document.body.appendChild(renderer.domElement);
 
-      renderer.setPixelRatio(window.devicePixelRatio);
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      renderer.localClippingEnabled = true;
-      document.body.appendChild(renderer.domElement);
+    const scene = new THREE.Scene();
 
-      const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      40,
+      window.innerWidth / window.innerHeight,
+      1,
+      200
+    );
 
-      const camera = new THREE.PerspectiveCamera(
-        40,
-        window.innerWidth / window.innerHeight,
-        1,
-        200
-      );
+    camera.position.set(-1.5, 2.5, 3.0);
 
-      camera.position.set(-1.5, 2.5, 3.0);
+    // control orbit camera and zoom in zoom out
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.addEventListener("change", () => renderer.render(scene, camera)); // use only if there is no animation loop
+    controls.minDistance = 1;
+    controls.maxDistance = 10;
+    controls.enablePan = false;
 
-      const controls = new OrbitControls(camera, renderer.domElement);
-      controls.addEventListener("change", () => renderer.render(scene, camera)); // use only if there is no animation loop
-      controls.minDistance = 1;
-      controls.maxDistance = 10;
-      controls.enablePan = false;
+    // lighting
+    const light = new THREE.HemisphereLight(0xffffff, 0x080808, 4.5);
+    light.position.set(-1.25, 1, 1.25);
+    scene.add(light);
 
-      const light = new THREE.HemisphereLight(0xffffff, 0x080808, 4.5);
-      light.position.set(-1.25, 1, 1.25);
-      scene.add(light);
+    // group circle
+    const group = new THREE.Group();
 
-      //
-
-      const group = new THREE.Group();
-
-      for (let i = 1; i <= 10; i += 2) {
-        const geometry = new THREE.SphereGeometry(i / 30, 48, 24);
-
-        const material = new THREE.MeshPhongMaterial({
-          color: new THREE.Color().setHSL(
-            Math.random(),
-            0.5,
-            0.5,
-            THREE.SRGBColorSpace
-          ),
-          side: THREE.DoubleSide,
-          clippingPlanes: clipPlanes,
-          clipIntersection: params.clipIntersection,
-          alphaToCoverage: true,
-        });
-
-        group.add(new THREE.Mesh(geometry, material));
-      }
-
-      scene.add(group);
-
-      // helpers
-
-      const helpers = new THREE.Group();
-      helpers.add(new THREE.PlaneHelper(clipPlanes[0], 2, 0xff0000));
-      helpers.add(new THREE.PlaneHelper(clipPlanes[1], 2, 0x00ff00));
-      helpers.add(new THREE.PlaneHelper(clipPlanes[2], 2, 0x0000ff));
-      helpers.visible = false;
-      scene.add(helpers);
-
-      // gui
-
-      const gui = new GUI();
-
-      gui.add(params, "alphaToCoverage").onChange(function (value) {
-        group.children.forEach((c: any) => {
-          c.material.alphaToCoverage = Boolean(value);
-          c.material.needsUpdate = true;
-        });
-
-        renderer.render(scene, camera);
+    for (let i = 1; i <= 30; i += 2) {
+      const geometry = new THREE.SphereGeometry(i / 30, 48, 24);
+      const material = new THREE.MeshPhongMaterial({
+        color: new THREE.Color().setHSL(
+          Math.random(),
+          0.5,
+          0.5,
+          THREE.SRGBColorSpace
+        ),
+        side: THREE.DoubleSide,
+        clippingPlanes: clipPlanes,
+        clipIntersection: params.clipIntersection,
+        alphaToCoverage: params.alphaToCoverage,
       });
 
-      gui
-        .add(params, "clipIntersection")
-        .name("clip intersection")
-        .onChange(function (value) {
-          const children = group.children;
-
-          for (let i = 0; i < children.length; i++) {
-            children[i].material.clipIntersection = value;
-          }
-
-          renderer.render(scene, camera);
-        });
-
-      gui
-        .add(params, "planeConstant", -1, 1)
-        .step(0.01)
-        .name("plane constant")
-        .onChange(function (value) {
-          for (let j = 0; j < clipPlanes.length; j++) {
-            clipPlanes[j].constant = value;
-          }
-
-          renderer.render(scene, camera);
-        });
-
-      gui
-        .add(params, "showHelpers")
-        .name("show helpers")
-        .onChange(function (value) {
-          helpers.visible = value;
-
-          renderer.render(scene, camera);
-        });
-
-      //
-
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-
-      renderer.setSize(window.innerWidth, window.innerHeight);
-
-      renderer.render(scene, camera);
+      group.add(new THREE.Mesh(geometry, material));
     }
 
-    init();
-  });
-  return <div></div>;
+    scene.add(group);
+
+    // helpers
+    const helpers = new THREE.Group();
+    helpers.add(new THREE.PlaneHelper(clipPlanes[0], 2, 0xff0000));
+    helpers.add(new THREE.PlaneHelper(clipPlanes[1], 2, 0x00ff00));
+    helpers.add(new THREE.PlaneHelper(clipPlanes[2], 2, 0x0000ff));
+    helpers.visible = true;
+    scene.add(helpers);
+
+    // gui
+    // control 3D element
+    const gui = new GUI();
+
+    gui.add(params, "alphaToCoverage").onChange(function (value) {
+      group.children.forEach((c: any) => {
+        c.material.alphaToCoverage = Boolean(value);
+        c.material.needsUpdate = true;
+      });
+      render();
+    });
+
+    gui.add(params, "clipIntersection").onChange(function (value) {
+      group.children.forEach((child: any) => {
+        child.material.clipIntersection = value;
+        child.material.needsUpdate = true; // Force material update
+      });
+      render();
+    });
+
+    gui
+      .add(params, "planeConstant", -1, 1)
+      .step(0.01)
+      .onChange(function (value) {
+        for (let j = 0; j < clipPlanes.length; j++) {
+          clipPlanes[j].constant = value;
+        }
+
+        render();
+      });
+
+    gui.add(params, "showHelpers").onChange(function (value) {
+      helpers.visible = value;
+      console.log("helpers => ", helpers.visible);
+
+      render();
+    });
+
+    window.addEventListener("resize", onWindowResize);
+
+    function onWindowResize() {
+      // add new aspect and update element size
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      render();
+    }
+
+    render();
+
+    return () => {
+      window.removeEventListener("resize", onWindowResize);
+      renderer.dispose();
+      gui.destroy();
+    };
+  }, [canvas.current]);
+
+  return (
+    <canvas ref={canvas} style={{ width: "100vw", height: "100vh" }}></canvas>
+  );
 }
 
 export default CirclePage;
